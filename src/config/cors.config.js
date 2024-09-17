0ff493230342277b81b2e98081c357aa7682a3cc
Brandon Quintero <brandonquinterogit@gmail.com>
2024-09-17 00:30:33 -0500

    AllowedHeaders Test 2

--- src/config/cors.config.js
+++ src/config/cors.config.js
@@ -1,11 +1,12 @@
 const allowedOrigins = [
-  "https://rcservice-front.netlify.app/",
+  "https://rcservice-front.netlify.app",
   "http://localhost:5173",
   "http://localhost:19006",
 ];
 const CORSConfiguration = {
   origin: allowedOrigins,
   credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
   methods: "*",
 };
 
