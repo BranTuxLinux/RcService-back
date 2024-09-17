b19e2dedbb92fabf3f076b49e229da8c09d2246b
Brandon Quintero <brandonquinterogit@gmail.com>
2024-09-17 00:24:30 -0500

    AllowedOrigin test

--- src/config/cors.config.js
+++ src/config/cors.config.js
@@ -1,12 +1,12 @@
 const allowedOrigins = [
-  "http://localhost:5173",
   "https://rcservice-front.netlify.app/",
+  "http://localhost:5173",
   "http://localhost:19006",
 ];
 const CORSConfiguration = {
   origin: allowedOrigins,
   credentials: true,
-  methods: "GET,PUT,POST,DELETE",
+  methods: "*",
 };
 
 module.exports = { CORSConfiguration };
